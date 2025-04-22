ef679ad555c570588bd95ddeea0b2cb6feed81ea
cafewang <434637878@qq.com>
2025-04-22 21:45:35 +0800

    sso within same domain

---
+++ messages-webapp/src/test/java/com/sivalabs/messages/two/MessagesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivalabs.messages.two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
